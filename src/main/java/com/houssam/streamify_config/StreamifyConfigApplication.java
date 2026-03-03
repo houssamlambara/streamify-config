56395e11c2d37afaaabb76461b6fef5c99bb50aa
houssam <lambarahoussam@gmail.com>
2026-03-03 10:55:48 +0000

    add structure

---
+++ src/main/java/com/houssam/streamify_config/StreamifyConfigApplication.java
@@ -0,0 +1,13 @@
+package com.houssam.streamify_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamifyConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamifyConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/houssam/streamify_config/StreamifyConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houssam.streamify_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamifyConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
